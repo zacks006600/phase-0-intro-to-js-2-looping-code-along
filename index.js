f48e3639ee5a92d3284545ed74047983e39be9ee
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-08-16 13:43:02 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = [`Lisa`, `Kaitlin`, `Jan`];
+
+function writeCards(names) {
+    let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    debugger;
+  }
+
+  return messages;
+}
+
+writeCards(names); 
+
+
+
+function countDown(){
+    let count = 10 
+    while (count > -1 ){
+        console.log(count--);
+    }
+}
